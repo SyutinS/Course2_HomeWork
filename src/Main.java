503aaec7428275fc501d121ce618345a2b082137
SyutinS <syut@mail.ru>
2023-05-05 16:08:22 +0300

    hw2.4.3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
-    public static void main(String[] args) { // метод с login, password, confirmPassword
+    public static void main(String[] args) {
         Users user = new Users("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
         System.out.println(user);
         Service.workExceptions(Users.getLogin(), Users.getPassword(), Users.getConfirmPassword());
     }
-
-
 }
--- src/Service.java
+++ src/Service.java
@@ -1,4 +1,4 @@
-public class Service { // login, password, confirmPassword
+public class Service {
 
     public static void workExceptions(String login, String password, String confirmPassword) {
         try {
