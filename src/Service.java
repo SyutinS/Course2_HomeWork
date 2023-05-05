b64bfeed1b2ad041b3752eb23d15de1db18de0a5
SyutinS <syut@mail.ru>
2023-05-05 16:24:44 +0300

    hw2.4.4

--- src/Service.java
+++ src/Service.java
@@ -5,7 +5,10 @@ public class Service {
             if (login == null || password == null || confirmPassword == null) {
                 throw new NullPointerException();
             }
-            if (!login.matches("[a-zA-Z0-9_]{1,19}")) { // неправильный логин
+            if (!login.matches("[a-zA-Z0-9_]+")) { // неправильный логин
+                throw new WrongLoginErrorException();
+            }
+            if (login.length() >= 20) {
                 throw new WrongLoginException();
             }
             if (!password.matches("[a-zA-Z0-9_]{1,20}")) { // неправильный пароль
@@ -16,6 +19,8 @@ public class Service {
             }
         } catch (NullPointerException nullError) {
             System.out.println(" проверить все обязательные поля: login, password или confirmPassword, что то забыли ввести. ");
+        } catch (WrongLoginErrorException loginEnterError) {
+            System.out.println("' " + login + " ' логин введен с ошибками, необходимо исправить");
         } catch (WrongLoginException loginError) {
             System.out.println(" WrongLoginException ");
         } catch (WrongPasswordErrorException passwordError) {
---
+++ src/WrongLoginErrorException.java
@@ -0,0 +1,2 @@
+public class WrongLoginErrorException extends Exception{
+}
